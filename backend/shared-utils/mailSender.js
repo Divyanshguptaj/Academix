3d6d8358019bfc59873c0acd7e8a3a444b674502
Divyanshguptaj <luckys510039@gmail.com>
2026-03-16 14:27:47 +0530

    fixed render.yaml changed port for node mailing

--- backend/shared-utils/mailSender.js
+++ backend/shared-utils/mailSender.js
@@ -5,8 +5,8 @@ const mailSender = async (email, title, body) => {
     // Create transporter
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT || 587,
-      secure: false, // true for 465, false for other ports
+      port: parseInt(process.env.MAIL_PORT) || 465,
+      secure: process.env.MAIL_SECURE === 'true',
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
