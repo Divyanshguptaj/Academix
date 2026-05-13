c4ab23eba8740e1b2da4652fd8f17850a52da317
Divyanshguptaj <luckys510039@gmail.com>
2026-05-13 14:11:23 +0530

    switched to http to send the mail using gmail api, google cloud and nodemailer

--- backend/shared-utils/mailSender.js
+++ backend/shared-utils/mailSender.js
@@ -3,24 +3,27 @@ import nodemailer from 'nodemailer'
 const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      service: 'gmail',
       auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        type: 'OAuth2',
+        user: process.env.MAIL_FROM,
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
       },
     })
 
     const info = await transporter.sendMail({
-      from: 'Academix | StudyNotion',
+      from: `Academix <${process.env.MAIL_FROM}>`,
       to: email,
       subject: title,
       html: body,
     })
 
-    console.log('Email sent successfully:', info.response)
+    console.log('Email sent successfully:', info.messageId)
     return info
   } catch (error) {
-    console.error('Error sending email:', error)
+    console.error('Error sending email:', error.message)
     throw error
   }
 }
